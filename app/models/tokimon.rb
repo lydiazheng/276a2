2ebc2cbc4dcf57a5633b8de21e7264d2d766bf05
Andy <hiandyzhang@gmail.com>
2016-10-11 00:57:10 +0000

    add model validation

--- app/models/tokimon.rb
+++ app/models/tokimon.rb
@@ -1,2 +1,10 @@
 class Tokimon < ApplicationRecord
+    belongs_to:trainer
+    validates :name, presence: true, length: { maximum: 50 }
+    validates :fly, :inclusion => {:in => [0,100]}
+    validates :fight, :inclusion => {:in => [0,100]}
+    validates :fire, :inclusion => {:in => [0,100]}
+    validates :water, :inclusion => {:in => [0,100]}
+    validates :electric, :inclusion => {:in => [0,100]}
+    validates :freeze, :inclusion => {:in => [0,100]}
 end
