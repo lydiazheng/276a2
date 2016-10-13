5b30d62c69a48e1282e347b96fce6d62c5e0480f
Andy <hiandyzhang@gmail.com>
2016-10-13 03:47:02 +0000

    change to nested route

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,21 @@ Rails.application.routes.draw do
   
   root 'trainers#index'
   
-  resources :trainers   do
-    member do
-      get :delete
-    end
-  end
+  # resources :trainers   do
+  #   member do
+  #     get :delete
+  #   end
+  # end
   
-    resources :tokimons   do
-    member do
-      get :delete
+  #   resources :tokimons   do
+  #   member do
+  #     get :delete
+  #   end
+      
+    resources :trainers do
+    resources :tokimons
     end
-    
+
   end
 
 
@@ -21,4 +25,3 @@ Rails.application.routes.draw do
   
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
